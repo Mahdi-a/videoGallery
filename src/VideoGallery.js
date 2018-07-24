ce52683fcfd7c30f751e93d2c17008dcd067d2e8
Mahdi Amirmazaheri <mahdi77@outlook.com>
2018-07-24 22:46:41 +1000

    creating structure and skeleton of the app

---
+++ src/VideoGallery.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+class VideoGallery extends Component {
+
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
+}
+
+export default VideoGallery
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import VideoGallery from './VideoGallery';
+
+ReactDOM.render(<VideoGallery />, document.getElementById('root'));
