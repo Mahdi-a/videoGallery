732be4bb8a9ebdfa24f1b8751c2e6b289bd5725c
Mahdi Amirmazaheri <mahdi77@outlook.com>
2018-07-26 00:35:06 +1000

    importing videos data to VideoGallery.js as state

--- src/VideoGallery.js
+++ src/VideoGallery.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import videosList from "./database/videos.js";
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+class VideoGallery extends React.Component {
 
-class VideoGallery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videosData: []
+        };
+        
+    }
 
-    render() {
+    componentWillMount() {
+        const data = {
+            videos: window.videos, function (video) {
+                // return JSON.stringify(video);
+            }
+        };
+        
+        const videosData = data.videos;
+        this.setState({ videosData });
+    }
+
+    componentDidMount() {
+
+        console.log("componentDidMount");
+        this.searchState();
+    }
+
+    searchState() {
+        console.log("Starting searching state");
+        const { videosData } = this.state;
+        // const test = videosData.find(w => w.id == 162702);        
+    }
+
+    render() {        
         return (
             <h1>Hello</h1>
         )
---
+++ src/Videos.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import React-DOM from "react-dom";
+// import videosList from "../database/videos.js";
+
+// class Videos extends React.Component {
+
+//     const videos = JSON.stringify(videosList)
+
+//     console.log(videos);
+
+//     render()
+    
+
+// }
